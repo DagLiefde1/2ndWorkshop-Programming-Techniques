808cc9eb75f5b8467702eb9f6f0b4c342614643a
DagLiefde1 <cadavidmartineztomas@gmail.com>
2024-04-19 11:53:13 -0500

    Adding video-views structures
    The exceptions were created and some changes were applied.

--- src/main/java/culturemedia/model/View.java
+++ src/main/java/culturemedia/model/View.java
@@ -1,24 +1,5 @@
 package culturemedia.model;
 import java.time.LocalDateTime;
 public record View(String userFullName, LocalDateTime startPlayingTime, Integer age, Video video) {
-        @Override
-        public String userFullName() {
-            return userFullName;
-        }
-
-        @Override
-        public LocalDateTime startPlayingTime() {
-            return startPlayingTime;
-        }
-
-        @Override
-        public Integer age() {
-            return age;
-        }
-
-        @Override
-        public Video video() {
-            return video;
-        }
 }
 

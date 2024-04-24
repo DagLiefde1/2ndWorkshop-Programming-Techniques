c83e42f2fe7f3075f178facc531f868859ee6653
DagLiefde1 <cadavidmartineztomas@gmail.com>
2024-04-24 18:49:49 -0500

    Adding test class

--- src/main/java/culturemedia/model/Video.java
+++ src/main/java/culturemedia/model/Video.java
@@ -1,5 +1,5 @@
 package culturemedia.model;
 
-public record Video(String code, String title, String description, Double duration){
+public record Video(String code, String title, String description, double duration){
 }
 
